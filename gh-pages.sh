a2af49a83249bdb4034967e4810f20ac3b0648a5
cailyn baksh <91929105+cailyn-baksh@users.noreply.github.com>
2021-11-28 14:34:26 -0700

    Update gh-pages.sh to work both ways

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-find . -type f -exec sed --regexp-extended -i "s|([\"'])(/.*\..*[\"'])|\\1/pdb-editor\\2|g" {} +
+if [ $1 == "push" ]; then
+	find . -type f -exec sed --regexp-extended -i "s|([\"'])(/.*\..*[\"'])|\\1/pdb-editor\\2|g" {} +
+elif [ $1 == "pull" ]; then
+	find . -type f -exec sed --regexp-extended -i "s|([\"'])/pdb-editor(/.*\..*[\"'])|\\1\\2|g" {} +
+else
+	echo Option must be one of push, pull
+fi
 
